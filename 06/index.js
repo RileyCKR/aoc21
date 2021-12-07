716d6db78a5f705ce91b97dab522561a555160e2
Chris Riley <criley@datanomix.io>
2021-12-07 16:17:12 -0500

    day 6 part2 solved
    use lossless fish compression to save memory on the fish array

--- 06/index.js
+++ 06/index.js
@@ -8,23 +8,36 @@ let INITIAL_SPAWN_PENALTY = 2
 
 class Pond {
   fishSchool
-  constructor(fishSchool) {
-    this.fishSchool = fishSchool
+  constructor(initialSchool) {
+    console.log(initialSchool)
+    // the index of the array reprents the age of the fish.  All fish of the same age
+    // will remain in the same groups which saves on memory space vs tracking fish
+    // individually.  In essence the fish are grouped by their spawn-age.
+    this.fishSchool = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    initialSchool.forEach(fishAge => {
+      this.fishSchool[fishAge]++
+    })
   }
 
   tick = () => {
-    let fishBabies = []
-    for(let i = 0; i < this.fishSchool.length; i++){
-      this.fishSchool[i]--
-      if (this.fishSchool[i] < 0) {
-        this.fishSchool[i] = SPAWN_COUNT
-        fishBabies.push(SPAWN_COUNT + INITIAL_SPAWN_PENALTY)
-      }
+    let fishBabies = this.fishSchool[0] // the number of newborn fish equals the number of 0 spawn-age fish at the start of the tick
+
+    // move fish down in the fishSchool array.  0 age fish move up to the spawn age
+    let zeroAgeFish = this.fishSchool[0]
+    for (let i=0; i < this.fishSchool.length; i++) {
+      this.fishSchool[i] = this.fishSchool[i+1]
     }
 
-    fishBabies.forEach(fish => {
-      this.fishSchool.push(fish)
-    })
+    this.fishSchool[SPAWN_COUNT] += fishBabies
+    this.fishSchool[SPAWN_COUNT + INITIAL_SPAWN_PENALTY] = fishBabies // fish are born
+  }
+
+  count = () => {
+    let sum = 0
+    for(let i=0; i < this.fishSchool.length; i++) {
+      sum += this.fishSchool[i]
+    }
+    return sum
   }
 }
 
@@ -35,8 +48,8 @@ let generations = 256
 
 for(let i=0; i < generations; i++){
   pond.tick()
-  console.log(`gen${i}, ${pond.fishSchool.length}`)
+  console.log(`gen${i}, ${pond.count()}`)
 }
 
-console.log('answer', pond.fishSchool.length)
+console.log('answer', pond.count())
 console.log('>=D')
